e654214b52fb8f6edf35fa2c17a5861f52b21555
Toohina <toohi2000@gmail.com>
2021-01-27 13:30:06 +0530

    added procfile and gitignore.

--- app.js
+++ app.js
@@ -205,6 +205,10 @@ app.get("/logout",function(req,res){
     res.redirect("/");
 });
 
-app.listen(3000,function(){
+let port=process.env.PORT;
+if(port==null||port==""){
+    port=3000;
+}
+app.listen(port,function(){
     console.log("Server has started.");
 });
